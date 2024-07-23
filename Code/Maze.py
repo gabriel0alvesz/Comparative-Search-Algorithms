8bd24655e6c9fb5fca027e363af1dd81ae74773d
Gabriel Alves <gabriel0alves.oficial@gmail.com>
2024-07-23 17:36:03 +0000

    maze created

--- Code/Maze.py
+++ Code/Maze.py
@@ -15,27 +15,30 @@ class Maze:
     def print_maze(self):
         for l in self._matrix_maze:
             for num in l:
-                print(f"{num} ",end="",)
+                print(f"{num}   ",end="",)
             print()
     
     
     def generate_obstacles(self):
         """Faz 60% da Matriz se tornar obstaculos"""    
         
-        line_num = random.randint(0, 10)
-        column_num = random.randint(0, 10)
+        cont: int = 0
+        aux_dict: dict = {(0,0): True, (9,9): True} # (0,0) -> começa no labirinto e (9,9) -> sai do labirinto
 
-        print(line_num)
-        print(column_num)
+        while cont < ((self.get_size()**2)*0.6):
+            line_num = random.randint(0, 9)
+            column_num = random.randint(0, 9)
+
+            if aux_dict.get((line_num,column_num)) != True:
+                cont += 1
+                self._matrix_maze[line_num][column_num] = -1
+                
          
-
 
 if __name__ == "__main__":
 
     labirinto = Maze(10)
 
-    print(labirinto.get_size())
-    # labirinto.print_maze()
-
     labirinto.generate_obstacles()
+    labirinto.print_maze()
     
